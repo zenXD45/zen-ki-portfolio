a42737e271b2455bebf60c131e259b295740e7f5
Parag Manwal <mparag838@gmail.com>
2025-09-01 13:19:24 +0000

    done

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Firebase Studio
+gi# Firebase Studio
 
 This is a NextJS starter in Firebase Studio.
 
--- src/components/zenitsu-loader.tsx
+++ src/components/zenitsu-loader.tsx
@@ -5,7 +5,7 @@ export function ZenitsuLoader() {
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-background">
       <div className="relative w-full h-full md:w-[50vw] md:h-[50vh] overflow-hidden">
         <video
-          src="https://media.tenor.com/P4z6y_b23g4AAAPo/zenitsu-agatsuma.mp4"
+          src="/videos/loader.mp4"
           autoPlay
           loop
           muted
